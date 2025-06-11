ab15ecba557d2235c237215e4e9860a9bc2cfdae
hphuc3005 <hphuc3005@gmail.com>
2025-06-11 23:06:23 +0700

    feat: update GHA

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -13,6 +13,9 @@ def pages_fixture():
     }
 
 
+def get_driver()
+
+
 class TestSetup:
     # This fixture is automatically used for the entire test class (scope="class", autouse=True)
     # It is responsible for setting up and tearing down the Selenium WebDriver for browser automation.
@@ -22,12 +25,13 @@ class TestSetup:
         session = requests.Session()
         request.cls.session = session
         # Set Chrome options for headless mode (for CI)
-        options = webdriver.ChromeOptions()
+        browser = request.config.getoption("--browser", default="chrome").capitalize()
+        options = getattr(webdriver, f"{browser}Otions")
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         # Initialize the Chrome WebDriver instance
-        driver = webdriver.Chrome(options=options)
+        driver = getattr(webdriver, browser)(options=options)
         # Maximize the browser window for better visibility during tests
         driver.maximize_window()
         # Attach the driver to the class instance (e.g., self.driver)
